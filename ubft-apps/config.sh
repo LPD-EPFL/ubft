38beea9480004b802b2be5a35ce3f8e775d4f601
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-10-18 18:26:05 +0200

    Update config.sh

--- ubft-apps/config.sh
+++ ubft-apps/config.sh
@@ -3,4 +3,4 @@ BINARIES_DIR=$1
 
 export DORY_LIB_REPARENT_PATH=$BINARIES_DIR/libreparent.so
 export LD_LIBRARY_PATH="$BINARIES_DIR${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-export SPDLOG_LEVEL=UbftServer=OFF,RequestLog=OFF,RpcClientRequestIngress=OFF
+export SPDLOG_LEVEL=CE=OFF,UbftServer=OFF,RequestLog=OFF,RpcClientRequestIngress=OFF
